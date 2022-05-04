b90c4cf42e37683f03035fd507305e7b7043f373
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-04 15:41:41 -0700

    updated admin dashboard page to include stats for categories and products, also added links

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,11 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD']
 
   def show
+    @products = Product.count
+    puts "number of products, #{@products}" 
+    @categories = Category.count
+    puts "number of categories, #{@categories}" 
+    @category = Category.all
+    p @category
   end
 end
