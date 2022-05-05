a17e640be48a41f572114794593dbd0ce2976b3e
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-05 13:48:33 -0700

    created session/user controllers, added sign in form, added log out login links to main nav bar, test login and register links

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,15 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :create, :new]
   end
 
+  #routes for login form, login in/log out
+  get "/login" => 'sessions#new'
+  post "/login" => 'sessions#create'
+  get "/logout" => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2022_05_05_193404) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_disgest"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
