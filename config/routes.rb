44c6f4778ca2cf31dda2acb0364b8beab72fdd71
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-05 14:15:55 -0700

    create sales route / added sales controller / added new view page for sale

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :create, :new]
+    resources :sales, only: [:index, :new]
   end
 
   #routes for login form, login in/log out
