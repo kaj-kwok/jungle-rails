d8b06bfec0085fa73bc87a095c63227c3744b792
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-05 02:14:21 -0700

    added submit button , added create to category controller, added validation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,25 @@
 class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD']
-
+  
   def index
     @categories = Category.order(id: :desc).all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)    
+    end
 end
