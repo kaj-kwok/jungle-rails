ee221580bd669397116921fa895c6c3675486671
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-05 23:17:04 -0700

    added helper functions to display discount codes

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,10 @@
 class Sale < ApplicationRecord
 
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+  
+
   def finished?
     ends_on < Date.current
   end
@@ -7,5 +12,5 @@ class Sale < ApplicationRecord
   def upcoming?
     ends_on > Date.current
   end
-  
+
 end
