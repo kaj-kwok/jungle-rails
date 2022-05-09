7886742ecc3aa0ce5d2abbafc852a85a520cea3c
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-08 18:22:22 -0700

    fixed issue with  not showing correctly

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :create, :new]
+    resources :categories, only: [:index, :create, :new, :destroy]
     resources :sales, only: [:index, :new]
   end
 
