71ef7e4c58022c7431fd6cdc84990888c4094833
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-08 22:58:22 -0700

    added number of products per category to admin page,

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -2,7 +2,4 @@ module SalesHelper
   def active_sale?
     Sale.active
   end
-  # def sale
-  #   s = Sale.where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
-  # end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,7 @@ class Category < ApplicationRecord
   has_many :products
   validates :name, :uniqueness => true
 
+  def number_of_products
+    products.size
+  end
 end
