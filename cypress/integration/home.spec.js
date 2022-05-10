d390421647c79d3fef3f8e9f1acd4cbb9cbebf66
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-10 13:02:40 -0700

    fixed bug with sale_active that was preventing test server from starting , added cypress tests for home-page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe("jungle app", () => {
+  it("should access the home page", () => {
+    cy.visit('localhost:3001');
+  });
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
