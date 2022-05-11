a19271daa8ebee6f78de0e57d4e056d937bacd03
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-10 21:12:22 -0700

    added validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
 
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating , presence: true, numericality: { only_integer: true, in: 1..5 }
 end
