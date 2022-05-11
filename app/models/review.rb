5bf4a1844854380aef89d608f30890cc0845b8e3
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-10 21:40:36 -0700

    added validation methods and tested methods in console for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
   validates :product_id, presence: true
   validates :user_id, presence: true
   validates :description, presence: true
-  validates :rating , presence: true, numericality: { only_integer: true, in: 1..5 }
+  validates :rating , presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
